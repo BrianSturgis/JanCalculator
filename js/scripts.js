d6a15ee33bd449b43461c31bcb42fef0f9c7df6a
Brian Sturgis <sturujisu@gmail.com>
2021-01-12 09:57:40 -0800

    add walrus show/hide project

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,7 @@
-// business logic
-function remainder(number1, number2) {
-  return number1 % number2;
-}
 
-function divide(number1, number2) {
-  return number1 / number2;
-}
-
-function multiply(number1, number2) {
-  return number1 * number2;
-}
-
-function subtract(number1, number2) {
-  return number1 - number2;
-}
-
-function add(number1, number2) {
-  return number1 + number2;
-}
-
-// user Logic
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = multiply(number1, number2);
-alert(result);
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
+});
